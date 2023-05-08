9b417dece5b815ef505b98cf87ca8c8715a284f5
Testare <testare.i9z@gmail.com>
2023-05-08 12:15:42 -0700

    WIP Gridmap

--- src/bevy_n/term.rs
+++ src/bevy_n/term.rs
@@ -2,9 +2,8 @@ use bevy::prelude::*;
 use crossterm::event::Event as CrosstermEvent;
 use crossterm::execute;
 use std::io::stdout;
-use std::ops::Deref;
 use std::panic;
-use std::sync::mpsc::{self, Receiver, RecvError, SyncSender, TryRecvError};
+use std::sync::mpsc::{self, Receiver, TryRecvError};
 use std::sync::Mutex;
 use std::time::Duration;
 
@@ -16,98 +15,14 @@ struct TerminalWindow {
     dimensions: Stub,
 }
 
-//Resource
 #[derive(Resource)]
 struct TermConfig {
     exit_key: char,
 }
 
-type Input = char;
-
-#[derive(Resource)]
+#[derive(Deref, Resource)]
 struct TermEventListener {
-    rx: Mutex<Receiver<Input>>,
-    cancel: SyncSender<()>,
-}
-
-impl Deref for TermEventListener {
-    type Target = Mutex<Receiver<Input>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.rx
-    }
-}
-
-impl Drop for TermEventListener {
-    fn drop(&mut self) {
-        println!("DROPPING");
-        self.cancel.send(());
-    }
-}
-
-struct InputEvent(Input);
-
-impl Default for TermConfig {
-    fn default() -> Self {
-        TermConfig { exit_key: 'q' }
-    }
-}
-
-impl Default for TermEventListener {
-    fn default() -> Self {
-        let (tx, rx) = mpsc::channel();
-        let (cancel, cancel_notification) = mpsc::sync_channel(0);
-        std::thread::spawn(move || {
-            let duration = Duration::from_millis(100);
-            loop {
-                if !matches!(cancel_notification.try_recv(), Err(TryRecvError::Empty)) {
-                    println!("LEZ BREAK");
-                    break;
-                }
-                match crossterm::event::poll(duration) {
-                    Ok(false) => {
-                        // If there is nothing ot read, we check for cancel notifications.
-                        // If it is empty and not disconnected, we continue looping, else we die.
-                        print!(".");
-                    }
-                    Err(e) => {
-                        log::error!(
-                            "Error occurred in crossterm listening thread while polling: {:?}",
-                            e
-                        );
-                        break;
-                    }
-                    Ok(true) => {
-                        match crossterm::event::read() {
-                            Ok(crossterm::event::Event::Key(key_event)) => {
-                                if let crossterm::event::KeyCode::Char(input) = key_event.code {
-                                    println!("Sending key: {}", input);
-                                    match tx.send(input) {
-                                        Ok(()) => {}
-                                        Err(mpsc::SendError(_)) => {
-                                            // Other end is dead, close this thread
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                            Ok(_) => {
-                                println!("Not a key event");
-                            }
-                            Err(e) => {
-                                println!("Error occurred reading {:?}", e);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        });
-        TermEventListener {
-            rx: Mutex::new(rx),
-            cancel,
-        }
-    }
+    rx: Mutex<Receiver<CrosstermEvent>>
 }
 
 impl TerminalWindow {
@@ -161,6 +76,54 @@ impl TerminalWindow {
     }
 }
 
+
+impl Default for TermConfig {
+    fn default() -> Self {
+        TermConfig { exit_key: 'q' }
+    }
+}
+
+impl Default for TermEventListener {
+    fn default() -> Self {
+        let (tx, rx) = mpsc::channel();
+        std::thread::spawn(move || {
+            let duration = Duration::from_millis(100);
+            loop {
+                match crossterm::event::poll(duration) {
+                    Ok(false) => {}
+                    Err(e) => {
+                        log::error!(
+                            "Error occurred in crossterm listening thread while polling: {:?}",
+                            e
+                        );
+                        break;
+                    }
+                    Ok(true) => {
+                        match crossterm::event::read() {
+                            Ok(event) => {
+                                match tx.send(event) {
+                                    Ok(()) => {}
+                                    Err(mpsc::SendError(_)) => {
+                                        // Other end is dead, close this thread
+                                        break;
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                log::error!("Error occurred reading crossterm events {:?}", e);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        TermEventListener {
+            rx: Mutex::new(rx)
+        }
+    }
+}
+
 impl Drop for TerminalWindow {
     fn drop(&mut self) {
         match Self::reset_terminal_state() {
@@ -178,11 +141,9 @@ impl Plugin for CharmiePlugin {
     fn build(&self, app: &mut App) {
         // TODO atty check
         app.add_startup_system(create_terminal_window);
-        app.add_event::<InputEvent>();
         app.add_event::<CrosstermEvent>();
         app.add_system(term_event_listener);
         app.add_system(exit_key);
-        println!("Hello from CharmiePlugin")
     }
 }
 
@@ -194,31 +155,33 @@ fn create_terminal_window(mut commands: Commands) {
     commands.init_resource::<TermConfig>();
     commands.init_resource::<TermEventListener>();
     commands.spawn(terminal_window);
-    println!("Hello!")
 }
 
 fn exit_key(
     term_config: Res<TermConfig>,
-    mut inputs: EventReader<InputEvent>,
+    mut inputs: EventReader<CrosstermEvent>,
     mut exit: EventWriter<bevy::app::AppExit>,
 ) {
     for input in inputs.iter() {
-        if input.0 == term_config.exit_key {
-            println!("Exit event");
-            exit.send(bevy::app::AppExit);
+        if let CrosstermEvent::Key(crossterm::event::KeyEvent { code, .. }) = input {
+            if *code == crossterm::event::KeyCode::Char(term_config.exit_key) {
+                exit.send(bevy::app::AppExit);
+            }
         }
     }
 }
 
-fn term_event_listener(term_listener: Res<TermEventListener>, mut inputs: EventWriter<InputEvent>) {
+fn term_event_listener(
+    term_listener: Res<TermEventListener>,
+    mut inputs: EventWriter<CrosstermEvent>,
+) {
     let lock = term_listener.try_lock();
     match lock {
         Ok(rx) => {
             loop {
                 match rx.try_recv() {
                     Ok(event) => {
-                        println!("Event received: {:?}", event);
-                        inputs.send(InputEvent(event))
+                        inputs.send(event);
                     }
                     Err(TryRecvError::Empty) => {
                         break;
