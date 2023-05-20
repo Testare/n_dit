61357cca543eb0161be40eb611a54187b44e2b90
Testare <testare.i9z@gmail.com>
2023-05-20 10:25:12 -0700

    Cache terminal rendering results to avoid flickering

--- src/term/render.rs
+++ src/term/render.rs
@@ -2,6 +2,7 @@ mod render_node;
 
 use std::io::{stdout, Write};
 
+use itertools::{Itertools, EitherOrBoth};
 use super::TerminalWindow;
 use bevy::core::FrameCount;
 use game_core::prelude::*;
@@ -41,6 +42,41 @@ impl Default for TerminalRendering {
     }
 }
 
+#[derive(Component, FromReflect, Reflect)]
+pub struct CachedTerminalRendering {
+    rendering: Vec<String>,
+    last_update: u32,
+}
+
+impl CachedTerminalRendering {
+    fn update_from(&mut self, tr: &TerminalRendering) {
+        self.rendering = tr.rendering.clone();
+        self.last_update = tr.last_update;
+    }
+}
+
+impl From<&TerminalRendering> for CachedTerminalRendering {
+    fn from(value: &TerminalRendering) -> Self {
+        CachedTerminalRendering {
+            rendering: value.rendering.clone(),
+            last_update: value.last_update,
+        }
+    }
+
+}
+
+impl PartialEq<TerminalRendering> for CachedTerminalRendering{
+    fn eq(&self, rhs: &TerminalRendering) -> bool {
+        self.rendering.iter().eq(rhs.rendering.iter())
+    }
+}
+
+impl PartialEq<CachedTerminalRendering> for TerminalRendering{
+    fn eq(&self, rhs: &CachedTerminalRendering) -> bool {
+        self.rendering.iter().eq(rhs.rendering.iter())
+    }
+}
+
 pub struct RenderPlugin;
 
 impl Plugin for RenderPlugin {
@@ -74,12 +110,36 @@ pub fn render_node(
 }
 
 pub fn write_rendering_to_terminal(
-    windows: Query<&TerminalWindow>,
+    mut commands: Commands,
+    mut windows: Query<(Entity, &TerminalWindow, Option<&mut CachedTerminalRendering>)>,
     renderings: Query<&TerminalRendering>,
 ) {
-    for window in windows.iter() {
+    for (window_entity, window, cached_rendering_opt) in windows.iter_mut() {
         // Future: get different write streams per window?
         if let Some(tr) = window.render_target.and_then(|id| renderings.get(id).ok()) {
+            match cached_rendering_opt {
+                Some(mut cached_rendering) => {
+                    if cached_rendering.as_ref() == tr {
+                        continue;
+                    }
+                    let render_result = render_with_cache(&tr.rendering, &cached_rendering.rendering);
+                    if let Result::Err(err) = render_result {
+                        log::error!("Error occurred in rendering: {:?}", err);
+                        continue;
+                    }
+                    cached_rendering.update_from(tr)
+                }, 
+                None => {
+                    let render_result = render_with_cache(&tr.rendering, &[]);
+                    if let Result::Err(err) = render_result {
+                        log::error!("Error occurred in rendering (no-cache): {:?}", err);
+                        continue;
+                    }
+                    commands.entity(window_entity).insert(CachedTerminalRendering::from(tr));
+                }
+            }
+            /*
+
             let mut stdout = stdout();
             // TODO logic to not render if not necessary
             crossterm::queue!(
@@ -95,7 +155,45 @@ pub fn write_rendering_to_terminal(
                     crossterm::cursor::MoveToColumn(0),
                 );
             }
-            stdout.flush();
+            stdout.flush();*/
         }
     }
+}
+
+
+/// Helper method
+fn render_with_cache(rendering: &[String], cached: &[String])-> std::io::Result<()> {
+    let mut stdout = stdout();
+    for (line_num, line) in rendering.iter().zip_longest(cached.iter()).enumerate() {
+        match line {
+            EitherOrBoth::Both(line_to_render, cached_line) => {
+                if line_to_render != cached_line {
+                    log::debug!("Changed cache line, rendering: {}", line_num);
+                    crossterm::queue!(
+                        stdout,
+                        crossterm::cursor::MoveTo(0, line_num as u16),
+                        crossterm::style::Print(line_to_render.clone()),
+                    )?;
+                }
+            },
+            EitherOrBoth::Left(line_to_render) => {
+                log::debug!("Rendering line without cache: {}", line_num);
+                crossterm::queue!(
+                    stdout,
+                    crossterm::cursor::MoveTo(0, line_num as u16),
+                    crossterm::style::Print(line_to_render.clone()),
+                )?;
+            },
+            EitherOrBoth::Right(_cached_line) => {
+                crossterm::queue!(
+                    stdout,
+                    crossterm::cursor::MoveTo(0, line_num as u16),
+                    crossterm::terminal::Clear(crossterm::terminal::ClearType::FromCursorDown)
+                )?;
+            }
+        }
+    }
+
+    crossterm::queue!(stdout, crossterm::cursor::MoveTo(0, 0))?;
+    stdout.flush()
 }
