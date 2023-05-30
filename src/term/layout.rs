92c575f61ee7f8a72e94a7abd27ea6a768289023
Testare <testare.i9z@gmail.com>
2023-05-29 23:05:51 -0700

    UNTESTED layout rendering logic

--- src/term/layout.rs
+++ src/term/layout.rs
@@ -1,6 +1,9 @@
 use crate::term::prelude::*;
+use bevy::core::FrameCount;
 use taffy::prelude::Style;
-use super::TerminalWindow;
+use unicode_width::UnicodeWidthStr;
+use super::{TerminalWindow, render::TerminalRendering};
+
 
 #[derive(Default)]
 pub struct TaffyTuiLayoutPlugin;
@@ -8,6 +11,8 @@ pub struct TaffyTuiLayoutPlugin;
 #[derive(Default, Deref, DerefMut, Resource)]
 pub struct Taffy(taffy::Taffy);
 
+#[derive(Component)]
+pub struct RenderedLayout;
 
 #[derive(Component, Debug, Deref, DerefMut)]
 pub struct TuiNode(taffy::node::Node);
@@ -36,7 +41,8 @@ impl Plugin for TaffyTuiLayoutPlugin {
             .add_systems((
             taffy_follow_entity_model,
             calculate_layouts,
-        ));
+            render_layouts,
+        ).chain());
     }
 }
 
@@ -92,5 +98,66 @@ fn calculate_layouts(
     for (node, name)  in tui_nodes.iter() {
         log::debug!("{} layout: {:?}", name.as_str(), taffy.layout(**node));
 
+    }
+}
+
+pub fn render_layouts(
+    taffy: Res<Taffy>,
+    frame_count: Res<FrameCount>,
+    mut render_layouts: Query<(Entity, &TuiNode, &mut TerminalRendering), With<RenderedLayout>>,
+    children: Query<&Children>,
+    child_renderings: Query<(&TuiNode, &TerminalRendering), Without<RenderedLayout>>
+) {
+    // do nothing
+    for (root_id, render_layout_node, mut rendering) in render_layouts.iter_mut() {
+        use pad::PadStr;
+        struct LeafInfo<'a> {
+            rendering: &'a TerminalRendering,
+            layout: &'a taffy::prelude::Layout,
+        }
+
+        let leaves = collect_leaves(root_id, &children);
+        let mut rendered_leaves: Vec<LeafInfo> = child_renderings.iter_many(leaves).map(|(node, rendering)|{
+            let layout = taffy.layout(**node).unwrap();
+            LeafInfo {
+                rendering,
+                layout
+            }
+        }).collect();
+        rendered_leaves.sort_by_cached_key(|leaf_info| {
+            (leaf_info.layout.location.x as u32, leaf_info.layout.location.y as u32)
+        });
+        
+        let root_layout = taffy.layout(**render_layout_node).unwrap();
+        let root_width = root_layout.size.width as usize;
+        let mut rows = vec![String::default() ; root_layout.size.height as usize];
+        for leaf in rendered_leaves {
+            let x_offset = leaf.layout.location.x as usize;
+            let y_offset = leaf.layout.location.y as usize;
+            for (i, child_row) in leaf.rendering.rendering().iter().enumerate() {
+                let row = &mut rows[i + y_offset];
+                let row_len = UnicodeWidthStr::width(row.as_str());
+                let new_row_string = format!(
+                    "{current_row}{space:padding$}{child_row}",
+                    current_row = row,
+                    space = " ",
+                    padding = x_offset - row_len,
+                    child_row = child_row);
+                *row = new_row_string;
+            }
+        }
+        let new_rendering = rows.into_iter().map(|row| row.pad_to_width(root_width)).collect();
+        rendering.update(new_rendering, frame_count.0);
+
+    }
+}
+
+// Helper function
+
+pub fn collect_leaves(root: Entity, children_query: &Query<&Children>) -> Vec<Entity> {
+    if let Ok(children) = children_query.get(root) {
+        children.into_iter().flat_map(|child|collect_leaves(*child, children_query)).collect()
+    } else {
+        vec![root]
     }
 }
