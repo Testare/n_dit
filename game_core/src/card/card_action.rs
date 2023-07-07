3a82d99b73c3588704e05fafcb8e9589cc894254
Testare <testare.i9z@gmail.com>
2023-07-06 21:52:31 -0700

    Charmie API extension: draw and clip

--- game_core/src/card/card_action.rs
+++ game_core/src/card/card_action.rs
@@ -38,7 +38,7 @@ impl ActionRange {
 }
 
 impl Prerequisite {
-    pub fn satisfied(&self, grid: &Mut<EntityGrid>, source: Entity, target: UVec2) -> bool {
+    pub fn satisfied(&self, grid: &Mut<EntityGrid>, source: Entity, _target: UVec2) -> bool {
         match self {
             Prerequisite::MinSize(min_size) => (*min_size as usize) <= grid.len_of(source),
         }
@@ -46,7 +46,7 @@ impl Prerequisite {
 }
 
 impl ActionEffect {
-    pub fn apply_effect(&self, grid: &mut Mut<EntityGrid>, source: Entity, target: UVec2) {
+    pub fn apply_effect(&self, grid: &mut Mut<EntityGrid>, _source: Entity, target: UVec2) {
         match self {
             ActionEffect::Damage(dmg) => {
                 if let Some(key) = grid.item_at(target) {
@@ -54,7 +54,7 @@ impl ActionEffect {
                     // TODO pop_back_n returns removed locations and old head as result for other systems
                 }
             },
-            ActionEffect::Heal(healing) => todo!("Healing not implemented yet"),
+            ActionEffect::Heal(_healing) => todo!("Healing not implemented yet"),
         }
     }
 }
--- src/charmie.rs
+++ src/charmie.rs
@@ -3,6 +3,7 @@ use std::fmt::Display;
 use std::ops::AddAssign;
 
 use crossterm::style::{ContentStyle, StyledContent};
+use unicode_width::{UnicodeWidthChar, UnicodeWidthStr};
 
 #[derive(Debug, Default)]
 pub enum ColorSupportLevel {
@@ -20,12 +21,70 @@ pub struct CharmieColor {
     basic: Option<String>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 struct CharacterMapImage {
     rows: Vec<CharmieRow>,
 }
 
-impl CharacterMapImage {}
+#[derive(Clone, Copy, Debug)]
+pub enum BrokenCharacterFillBehavior {
+    Char(char),
+    Gap,
+}
+
+impl Default for BrokenCharacterFillBehavior {
+    fn default() -> Self {
+        Self::Char(' ')
+    }
+}
+
+impl CharacterMapImage {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    // Draws map onto this image, expanding the image as necessary, at the location specified.
+    pub fn draw(
+        &self,
+        map: &CharacterMapImage,
+        x: u32,
+        y: u32,
+        bcfb: BrokenCharacterFillBehavior,
+    ) -> Self {
+        let y = y as usize;
+        let mut result = self.clone();
+
+        if result.rows.len() < (y + map.rows.len()) {
+            result
+                .rows
+                .resize(y + map.rows.len(), CharmieRow::default());
+        }
+        for (row_index, row) in map.rows.iter().enumerate() {
+            let row_index = row_index + y as usize;
+            result.rows[row_index] = self.rows[row_index].draw(row, x, bcfb);
+        }
+        result
+    }
+
+    pub fn clip(
+        &self,
+        x: u32,
+        y: u32,
+        width: u32,
+        height: u32,
+        bcfb: BrokenCharacterFillBehavior,
+    ) -> Self {
+        CharacterMapImage {
+            rows: self
+                .rows
+                .iter()
+                .skip(y as usize)
+                .take(height as usize)
+                .map(|row| row.clip(x, width, bcfb))
+                .collect(),
+        }
+    }
+}
 
 impl From<CharacterMapImage> for Vec<String> {
     fn from(value: CharacterMapImage) -> Self {
@@ -48,15 +107,26 @@ struct CharmieRow {
 }
 
 impl CharmieRow {
-    pub fn add_gap(&mut self, len: u32) {
+    pub fn len(&self) -> u32 {
+        self.segments
+            .iter()
+            .map(|segment| {
+                let len = segment.len();
+                len
+            })
+            .sum()
+    }
+
+    pub fn add_gap(&mut self, len: u32) -> &mut Self {
         if let Some(CharmieSegment::Empty { len: last_len }) = self.segments.last_mut() {
             *last_len += len;
         } else {
             self.segments.push(CharmieSegment::Empty { len });
         }
+        self
     }
 
-    pub fn add_text<S: Borrow<str>>(&mut self, text: S, text_format: &ContentStyle) {
+    pub fn add_text<S: Borrow<str>>(&mut self, text: S, text_format: &ContentStyle) -> &mut Self {
         match self.segments.last_mut() {
             Some(CharmieSegment::Textual {
                 text: last_text,
@@ -69,10 +139,145 @@ impl CharmieRow {
                 });
             },
         }
+        self
     }
 
-    pub fn add_styled_text<D: Display>(&mut self, styled_content: StyledContent<D>) {
-        self.add_text(styled_content.content().to_string(), styled_content.style())
+    pub fn add_styled_text<D: Display>(&mut self, styled_content: StyledContent<D>) -> &mut Self {
+        self.add_text(styled_content.content().to_string(), styled_content.style());
+        self
+    }
+
+    pub fn with_gap(mut self, len: u32) -> Self {
+        self.add_gap(len);
+        self
+    }
+
+    pub fn with_text(mut self, text: &str, text_format: &ContentStyle) -> Self {
+        self.add_text(text, text_format);
+        self
+    }
+
+    pub fn with_styled_text<D: Display>(mut self, styled_content: StyledContent<D>) -> Self {
+        self.add_styled_text(styled_content);
+        self
+    }
+
+    pub fn draw(&self, row: &CharmieRow, x: u32, bcfb: BrokenCharacterFillBehavior) -> Self {
+        println!("Drawing {:?} to {:?}", row, self);
+        let mut result = if x > 0 {
+            self.clip(0, x, bcfb)
+        } else {
+            CharmieRow::default()
+        };
+        let self_len = self.len();
+        if self_len < x {
+            result.add_gap(x - self_len);
+        }
+
+        let mut result_len = result.len();
+        for segment in row.segments.iter() {
+            match segment {
+                CharmieSegment::Empty { .. } => {
+                    let under_clip = self.clip(result_len, segment.len(), bcfb);
+                    let clip_len = under_clip.len();
+                    result += &under_clip;
+                    if clip_len < segment.len() {
+                        result.add_gap(segment.len() - clip_len);
+                    }
+                },
+                CharmieSegment::Textual { .. } => {
+                    result += segment;
+                },
+            }
+            result_len += segment.len();
+        }
+
+        // result += row; // Will need to fill gaps from below, that'll come later
+
+        let result_len = result.len();
+        if result_len < self_len {
+            result += &self.clip(result_len, self_len - result_len, bcfb);
+        }
+        result
+    }
+
+    /// Note: FillBehavior only applies to character that get cut in half.
+    /// It does not fill in empty space in the clip, such as if the width is bigger than the source's width
+    pub fn clip(&self, clip_start: u32, width: u32, bcfb: BrokenCharacterFillBehavior) -> Self {
+        let clip_end = clip_start + width;
+        self.segments
+            .iter()
+            .fold(
+                (0, CharmieRow::default()),
+                |(seg_start, mut row), segment| {
+                    let seg_end = seg_start + segment.len();
+                    if seg_start < clip_end && clip_start < seg_end {
+                        if seg_start >= clip_start && clip_end >= seg_end {
+                            row += segment;
+                        } else {
+                            let skip_start = clip_start.saturating_sub(seg_start);
+                            let take_until = clip_end.saturating_sub(seg_start).min(segment.len());
+                            let len = take_until - skip_start;
+                            match segment {
+                                CharmieSegment::Empty { .. } => {
+                                    row += &CharmieSegment::Empty { len }
+                                },
+                                CharmieSegment::Textual { text, style } => {
+                                    let text: String = text
+                                        .chars()
+                                        .scan(seg_start, |index, ch| {
+                                            let current_idx = *index;
+                                            let char_width = ch.width().unwrap_or(0) as u32;
+                                            let next_idx = current_idx + char_width;
+                                            *index = next_idx;
+
+                                            if next_idx <= skip_start {
+                                                return Some(None); // Haven't gotten to the clip yet
+                                            } else if current_idx >= take_until {
+                                                return None; // Clip is done
+                                            }
+
+                                            if char_width == 2 {
+                                                if current_idx + 1 == skip_start {
+                                                    // Full width character sliced in half at the beginning
+                                                    match bcfb {
+                                                        BrokenCharacterFillBehavior::Char(fill_ch) => {
+                                                            return Some(Some(fill_ch))
+                                                        },
+                                                        BrokenCharacterFillBehavior::Gap => {
+                                                            row +=
+                                                                &CharmieSegment::Empty { len: 1 };
+                                                            return Some(None);
+                                                        },
+                                                    }
+                                                } else if current_idx + 1 == take_until {
+                                                    // Full width character sliced in half at the end
+                                                    match bcfb {
+                                                        BrokenCharacterFillBehavior::Char(fill_ch) => {
+                                                            return Some(Some(fill_ch))
+                                                        },
+                                                        BrokenCharacterFillBehavior::Gap => {
+                                                            return None;
+                                                        },
+                                                    }
+                                                }
+                                            }
+                                            Some(Some(ch))
+                                        })
+                                        .filter_map(|c| c)
+                                        .collect();
+                                    row += CharmieSegment::Textual {
+                                        text,
+                                        style: *style,
+                                    };
+                                },
+                            }
+                        }
+                    }
+                    (seg_start + segment.len(), row)
+                },
+            )
+            .1
     }
 }
 
@@ -117,14 +322,32 @@ impl<D: Display> AddAssign<StyledContent<D>> for CharmieRow {
 impl AddAssign<&CharmieRow> for CharmieRow {
     fn add_assign(&mut self, rhs: &CharmieRow) {
         for segment in rhs.segments.iter() {
-            match segment {
-                CharmieSegment::Empty { len } => self.add_gap(*len),
-                CharmieSegment::Textual {
-                    text,
-                    style: format,
-                } => self.add_text(text.as_str(), format),
-            }
+            *self += segment;
         }
+    }
+}
+
+impl AddAssign<&CharmieSegment> for CharmieRow {
+    fn add_assign(&mut self, rhs: &CharmieSegment) {
+        match rhs {
+            CharmieSegment::Empty { len } => self.add_gap(*len),
+            CharmieSegment::Textual {
+                text,
+                style: format,
+            } => self.add_text(text.as_str(), format),
+        };
+    }
+}
+
+impl AddAssign<CharmieSegment> for CharmieRow {
+    fn add_assign(&mut self, rhs: CharmieSegment) {
+        match rhs {
+            CharmieSegment::Empty { len } => self.add_gap(len),
+            CharmieSegment::Textual {
+                text,
+                style: format,
+            } => self.add_text(text.as_str(), &format),
+        };
     }
 }
 
@@ -136,6 +359,16 @@ impl AddAssign<&CharmieRow> for CharmieRow {
 enum CharmieSegment {
     Textual { text: String, style: ContentStyle },
     Empty { len: u32 },
+    // Effect { len: u32, style: ContentStyle},
+}
+
+impl CharmieSegment {
+    fn len(&self) -> u32 {
+        match self {
+            Self::Textual { text, .. } => text.width() as u32,
+            Self::Empty { len } => *len,
+        }
+    }
 }
 
 impl Display for CharmieSegment {
@@ -167,5 +400,64 @@ impl<D: Display> From<StyledContent<D>> for CharmieSegment {
             text: value.content().to_string(),
             style: *value.style(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_clipping_charmie_row_with_fullwidth_characters() {
+        let row = CharmieRow::from("世Hello界world".to_string());
+
+        let clipped = row.clip(0, 9, BrokenCharacterFillBehavior::Gap);
+        assert_eq!(clipped.to_string(), "世Hello界");
+
+        let clipped = row.clip(1, 7, BrokenCharacterFillBehavior::Char('x'));
+        assert_eq!(clipped.to_string(), "xHellox");
+
+        let clipped = row.clip(1, 7, BrokenCharacterFillBehavior::Gap);
+        assert_eq!(clipped.to_string(), " Hello"); // Gap is not added to the end, but at the beginning
+    }
+
+    #[test]
+    fn test_draw_charmie_row() {
+        let row = CharmieRow::from("世Hello界world".to_string());
+        let draw_row = CharmieRow::from("Mimsy".to_string());
+        let gap_draw_row = CharmieRow::from("[".to_string())
+            .with_gap(2)
+            .with_text("]", &Default::default());
+
+        let drawing = row.draw(&draw_row, 0, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "Mimsylo界world");
+
+        let drawing = row.draw(&draw_row, 1, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "_Mimsyo界world");
+
+        let drawing = row.draw(&draw_row, 2, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世Mimsy界world");
+
+        let drawing = row.draw(&draw_row, 3, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世HMimsy_world");
+
+        let drawing = row.draw(&draw_row, 14, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世Hello界worldMimsy");
+
+        let drawing = row.draw(&draw_row, 16, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世Hello界world  Mimsy");
+
+        let drawing = row.draw(&gap_draw_row, 0, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "[_H]llo界world");
+        let drawing = row.draw(&gap_draw_row, 1, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "_[He]lo界world");
+        let drawing = row.draw(&gap_draw_row, 2, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世[el]o界world");
+        let drawing = row.draw(&gap_draw_row, 6, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世Hell[界]orld");
+        let drawing = row.draw(&gap_draw_row, 14, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世Hello界world[  ]");
+        let drawing = row.draw(&gap_draw_row, 16, BrokenCharacterFillBehavior::Char('_'));
+        assert_eq!(drawing.to_string(), "世Hello界world  [  ]");
     }
 }
