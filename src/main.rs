779c0a40562749c88bcb8ed5be5d6e4ab703bcb1
Testare <testare.i9z@gmail.com>
2023-12-19 07:38:48 -0800

    clap for CLI args

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,47 @@ use std::time::Duration;
 
 use bevy::prelude::*;
 use bevy::scene::ScenePlugin;
+use clap::Parser;
 use cq_term::demo::DemoNodeId;
 use game_core::node::NodeId;
 use simplelog::{LevelFilter, WriteLogger};
 
-fn main() {
-    setup_logging();
-    let mut demo_node_id = DemoNodeId(None);
-    for arg in std::env::args() {
-        if arg == "n0" {
-            demo_node_id.0 = Some(NodeId::new("node:demo", 0));
-        } else if arg == "n1" {
-            demo_node_id.0 = Some(NodeId::new("node:tutorial", 0));
-        } else if arg == "n2" {
-            demo_node_id.0 = Some(NodeId::new("node:tutorial", 1));
-        } else if arg == "n3" {
-            demo_node_id.0 = Some(NodeId::new("node:tutorial", 2));
-        }
-    }
+#[derive(Parser)]
+#[command(author, version, about)]
+struct CqCliPlugin {
+    /// Select a demo node to load. Currently 0-3 supported
+    #[arg(short, long, value_name = "NODE #")]
+    node: Option<u8>,
+    /// Activates logging and debuging to local file.
+    #[arg(short, long)]
+    debug: bool,
+    /// Increases debug logging to next leve
+    #[arg(short, long)]
+    trace: bool,
+    /// Specifies a server to connect to. Not currently implemented
+    #[arg(short, long, value_name = "SERVER ADDRESS")]
+    connect: Option<String>,
+}
 
+impl Plugin for CqCliPlugin {
+    fn build(&self, app: &mut App) {
+        let demo_node_id = DemoNodeId(self.node.and_then(|node_num| match node_num {
+            0 => Some(NodeId::new("node:demo", 0)),
+            1 => Some(NodeId::new("node:tutorial", 0)),
+            2 => Some(NodeId::new("node:tutorial", 1)),
+            3 => Some(NodeId::new("node:tutorial", 2)),
+            _ => None,
+        }));
+        app.insert_resource(demo_node_id);
+    }
+}
+
+fn main() {
+    let cq_cli = CqCliPlugin::parse();
+    setup_logging(&cq_cli);
     App::new()
-        .insert_resource(demo_node_id)
         .add_plugins((
+            cq_cli,
             AssetPlugin { ..default() },
             HierarchyPlugin,
             bevy::audio::AudioPlugin::default(),
@@ -42,14 +61,14 @@ fn main() {
 }
 
 // Can set up more advanced CLI support in the future with clap
-fn setup_logging() {
-    if std::env::args().any(|arg| arg == "--debug") {
-        let file = if std::env::args().any(|arg| arg == "--connect") {
+fn setup_logging(cq_cli: &CqCliPlugin) {
+    if cq_cli.debug {
+        let file = if cq_cli.connect.is_some() {
             "debug.connect.log"
         } else {
             "debug.log"
         };
-        let log_level: LevelFilter = if std::env::args().any(|arg| arg == "--trace") {
+        let log_level: LevelFilter = if cq_cli.trace {
             LevelFilter::Trace
         } else {
             LevelFilter::Debug
