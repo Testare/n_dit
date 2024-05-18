a68254e85d30cac65a12ac5d40a836a227980525
Testare <testare.i9z@gmail.com>
2024-05-17 18:27:03 -0700

    Try new fix for audio bug

--- cq_term/src/demo.rs
+++ cq_term/src/demo.rs
@@ -228,12 +228,12 @@ fn demo_startup(
     // if no audio is playing, we are adding "background music" of
     // silence.
     // See issue: https://github.com/bevyengine/bevy/issues/9798
-    // NOTE: This unfortunately did not work.
+    // NOTE: This unfortunately did not work when playing a silent audio file, trying a real audio file with 0 volume
     commands.spawn(AudioBundle {
-        source: asset_server.load("audio/1-second-of-silence.mp3"),
+        source: asset_server.load("tmp/audio/mixkit-coins-sound-2003.wav"),
         settings: PlaybackSettings {
             mode: bevy::audio::PlaybackMode::Loop,
-            volume: Volume::new(10.0),
+            volume: Volume::new(0.0),
             ..default()
         },
     });
